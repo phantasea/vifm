2b86a22a7d179aca26868a98958f2eccd2d67829
xaizek <xaizek@posteo.net>
2026-07-24 20:30:21 +0300

    Add missing NULL check to matchers_alloc()

--- src/utils/matchers.c
+++ src/utils/matchers.c
@@ -81,8 +81,14 @@ matchers_t *
 matchers_alloc(const char expr[], const char list[], int cs_by_def,
 		int glob_by_def, const char on_empty_re[], char **error)
 {
+	*error = NULL;
+
 	int nsubs;
 	char **subs = break_into_matchers(list, &nsubs, /*is_list=*/0);
+	if(subs == NULL)
+	{
+		return NULL;
+	}
 
 	MatcherExpr expr_kind = (glob_by_def ? ME_DEF_GLOB : ME_DEF_REGEX);
 	matchers_t *matchers = matchers_init(expr, subs, nsubs, cs_by_def,
